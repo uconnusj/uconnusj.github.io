db9f32ade343113c2aae960750c07fb823748649
Anshul Rastogi <stellaranshul@gmail.com>
2024-03-05 16:03:22 -0500

    Fixed members page Fixed FAQ page

---
+++ html/members-info.js
@@ -0,0 +1,112 @@
+const roles={
+    eboard:"Editor-in-Chief",
+    editorial:{
+        member:"Editorial Team",
+        eboard:"Executive Editor"
+    },
+    journalism:{
+        member:"Jounralism Team",
+        eboard:"Director of Journalism"
+    },
+    treasury:{
+        member:"Treasury Team",
+        eboard:"Director of Outreach"
+    },
+    technology:{
+        member:"Technology Team",
+        eboard:"Head of Web Development"
+    },
+    advertisement:{
+        member:"Advertisement Team",
+        eboard:"Director of Advertising"
+    }
+}
+
+// Assuming you have an array of people objects called 'members'
+const members = [
+    {
+        name: "Sofya Levitina",
+        position: roles.eboard,
+        imageSrc: "sopha_eboard.jpeg"
+    },
+    {
+        name: "Joseph Mooney",
+        position: roles.treasury.eboard,
+        imageSrc: "joe_eb.jpeg"
+    },
+    {
+        name: "Malak Nechnach",
+        position: roles.journalism.member,
+        imageSrc: "malak.png"
+    },
+    {
+        name: "Yarden Sackett",
+        position: roles.treasury.member,
+        imageSrc: "yarden.png"
+    },
+    {
+        name: "Christian Bjork",
+        position: roles.editorial.eboard,
+        imageSrc: "christian_eb.png"
+    },
+    {
+        name: "Laura Augenbraun",
+        position: roles.advertisement.eboard,
+        imageSrc: ""  // Add the correct image source
+    },
+    {
+        name: "Devesh Karthik",
+        position: roles.journalism.member,
+        imageSrc: "devesh.jpeg"
+    },
+    {
+        name: "Rilee Harris",
+        position: roles.editorial.member,
+        imageSrc: "rilee.jpeg"
+    },
+    {
+        name: "John-Michael Mendez",
+        position: roles.technology.member,
+        imageSrc: ""  // Add the correct image source
+    },
+    {
+        name: "Siddharth Sinha",
+        position: roles.technology.eboard,
+        imageSrc: "siddharth_eb.jpg"
+    },
+    {
+        name: "Natalie Aloisio",
+        position: roles.editorial.member,
+        imageSrc: "natalie.jpeg"
+    },
+    {
+        name: "Ryan Durrell",
+        position: roles.journalism.member,
+        imageSrc: "ryan.jpeg"
+    },
+    {
+        name: "Sravya Lingam",
+        position: roles.advertisement.member,
+        imageSrc: ""  // Add the correct image source
+    },
+    {
+        name: "Danya Alboslani",
+        position: roles.journalism.member,
+        imageSrc: "danya.jpeg"
+    },
+    {
+        name: "Suheera Haq",
+        position: roles.advertisement.eboard,
+        imageSrc: "suheera_eb.jpeg"
+    },
+    {
+        name: "Enya Goonetilleke",
+        position: roles.editorial.member,
+        imageSrc: "enya.jpeg"
+    },
+    // Add more member objects as needed...
+];
+
+if (members.imageSrc=="") {
+    members.imageSrc="BLANK.png";
+  }
---
+++ includes/includes.js
@@ -0,0 +1,14 @@
+//Navbar
+$(function() {
+    $("#includeNavbar").load("/uconnusj.github.io/includes/navbar.html");
+});
+
+//Style
+$(function() {
+    $("#includeStyle").load("/uconnusj.github.io/includes/style.html");
+});
+
+//Footer
+$(function() {
+    $("#includeFooter").load("/uconnusj.github.io/includes/footer.html");
+});
